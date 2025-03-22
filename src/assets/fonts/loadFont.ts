48e07774bd20cd0b59e3c643e52829774005b891
Harry Duncan <h.duncan@live.com>
2025-03-22 11:11:53 +1100

    remove loaded font

--- src/assets/fonts/loadFont.ts
+++ src/assets/fonts/loadFont.ts
@@ -1,9 +1,11 @@
-import { FontData, FontLoader } from "three/examples/jsm/loaders/FontLoader";
+//import { FontData, FontLoader } from "three/examples/jsm/loaders/FontLoader";
 
-export const loadFont = (fontUrl: string) =>
-  new Promise((resolve: (value: FontData) => void) => {
-    const loader = new FontLoader();
-    loader.load(fontUrl, (response) => {
-      resolve(response.data);
-    });
-  });
+export const loadFont = (fontUrl: string) => {
+  return null;
+};
+// new Promise((resolve: (value: FontData) => void) => {
+//   const loader = new FontLoader();
+//   loader.load(fontUrl, (response) => {
+//     resolve(response.data);
+//   });
+//});
