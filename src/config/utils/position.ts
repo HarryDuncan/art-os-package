37a94b48e8ed8d9d6cb10091dc5a93db9e34efec
Harry Duncan <h.duncan@live.com>
2025-06-08 14:37:51 +1000

    remove unused files

--- src/config/utils/position.ts
+++
@@ -1,9 +0,0 @@
-import { Position3d } from "../../types/position.types";
-
-export const getPosition = (positionConfig: Partial<Position3d>) => {
-  return {
-    x: positionConfig.x ?? 0,
-    y: positionConfig.y ?? 0,
-    z: positionConfig.z ?? 0,
-  };
-};
