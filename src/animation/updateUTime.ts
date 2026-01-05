9d16590363f7bce88809e422f9af655708aeafcf
Harry Duncan <h.duncan@live.com>
2026-01-06 08:38:32 +1100

    update uTime for point meshes too

--- src/animation/updateUTime.ts
+++ src/animation/updateUTime.ts
@@ -1,10 +1,10 @@
-import { Mesh, RawShaderMaterial } from "three";
+import { Mesh, Points, RawShaderMaterial } from "three";
 import { InteractiveScene } from "../components/interactive-scene/InteractiveScene";
 
 export const updateUTime = (scene: InteractiveScene) => {
   const time = scene.clock?.getElapsedTime() ?? 0;
   scene.children.forEach((child) => {
-    if (child instanceof Mesh) {
+    if (child instanceof Mesh || child instanceof Points) {
       const material = child.material as RawShaderMaterial;
       material.uniforms.uTime.value = time;
     }
